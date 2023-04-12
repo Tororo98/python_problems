fff71520107bc25ee130e72b0b406c117c68157a
Tororo98 <sebastiantofra98@javerianacali.edu.co>
2023-04-12 10:12:24 -0500

    Documentation for the code

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,23 @@
 def isPalindrome(x):
+    # Declaration of variables for the problem
     flag = False
     digits = []
     revDigits = []
+
+    # store digits in array
     if x>0:
         while(x>0):
             digit,x = x%10, x//10
             digits.append(digit)
+
+        # Reverse digits
         for item in reversed(digits):
             revDigits.append(item)
+
+        # Chekc if number is palindrome or not
         if(revDigits==digits):
             flag = True
-    else:
-        return flag
+
     return flag
 
-print(isPalindrome(123))
+print(isPalindrome(121))
