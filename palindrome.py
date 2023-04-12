dcb66cd8ca70ff2f75ee32eb182bb071ec0ea47f
Tororo98 <sebastiantofra98@javerianacali.edu.co>
2023-04-12 10:08:09 -0500

    Finished palindrome prob, not optimal

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,17 @@
 def isPalindrome(x):
     flag = False
     digits = []
+    revDigits = []
     if x>0:
         while(x>0):
             digit,x = x%10, x//10
             digits.append(digit)
-
-        if(digits.reverse()==digits):
+        for item in reversed(digits):
+            revDigits.append(item)
+        if(revDigits==digits):
             flag = True
     else:
         return flag
     return flag
 
-print(isPalindrome(121))
+print(isPalindrome(123))
